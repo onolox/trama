4fb465d7d28732cc10e755440c62108432843895
onolox <onolox@0a8083cf-0a4d-0410-8dbd-794603ec2690>
2008-05-12 18:57:04 +0000

    primeira vez

---
+++ trunk/JavaApplication4/src/RendererLinha.java
@@ -0,0 +1,25 @@
+
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+public class RendererLinha extends DefaultTableCellRenderer {
+    public RendererLinha() {
+        super();
+    }
+
+    @Override
+    public Component getTableCellRendererComponent( JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column ) {
+
+        JLabel label = ( JLabel ) super.getTableCellRendererComponent( table, value, isSelected, hasFocus, row, column );
+
+        label.setHorizontalAlignment( RIGHT );
+        label.setOpaque( true );
+        label.setBackground( javax.swing.UIManager.getDefaults().getColor( "Button.light" ) );
+        label.setBorder( new javax.swing.border.SoftBevelBorder( javax.swing.border.BevelBorder.RAISED ) );
+
+
+        return label;
+    }
+}
