4abdf064d08c3fd569230e1bba231fee155b4b17
onolox <onolox@0a8083cf-0a4d-0410-8dbd-794603ec2690>
2008-09-11 17:33:07 +0000

    ainda otimizando as parada

--- Trama/src/visao/renderizador/RenderizadorCelula.java
+++ Trama/src/visao/renderizador/RenderizadorCelula.java
@@ -28,32 +28,32 @@ public class RenderizadorCelula extends DefaultTableCellRenderer {
 	@Override
 	public Component getTableCellRendererComponent( JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column ) {
 		JTableCustomizado tab = ( JTableCustomizado ) table;
-       
+       int v = Integer.parseInt( value.toString());
 		
         if( column == tab.getColunaAtual() && row < tab.getLinhaAtual() ) setBackground( color1 );
 		else if( row == tab.getLinhaAtual() && column < tab.getColunaAtual() ) setBackground( color1 );
 		else setBackground( Color.WHITE );
 		
-		if( value.equals( "0" ) ) setText( "" );
-		else if( value.equals( "1" ) ){
+		if( v == 0 ) setText( "" );
+		else if( v == 1 ){
 		    setText( "X" );
 			setBackground( color2 );
-		} else if( value.equals( "2" ) ){
+		} else if( v == 2 ){
 			setText( "X" );
 			setBackground( color3 );
-		} else if( value.equals( "3" ) ){
+		} else if( v == 3){
 			setText( "X" );
 			setBackground(color4 );
-		} else if( value.equals( "4" ) ){
+		} else if( v == 4 ){
 			setText( "X" );
 			setBackground( color5 );
-		} else if( value.equals( "20" ) ){
+		} else if( v == 20 ){
 		setText( "" );
 			setBackground( color3 );
-		} else if( value.equals( "30" ) ){
+		} else if( v == 30 ){
 			setText( "" );
 			setBackground( color4 );
-		} else if( value.equals( "40" ) ){
+		} else if(v == 40 ){
 			setText( "" );
 			setBackground( color5 );
 		}
